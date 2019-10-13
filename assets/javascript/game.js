1c9c7d69446914a6975e3a4e1156aaa4821c6606
Jeremy Gill <jeremygill87@gmail.com>
2019-10-13 00:19:50 -0400

    Javascript is working and functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,104 @@
 $(document).ready(function() {
 
-    var triviaBlocks = [
+    $("#play").on("click", function() {
+        game.displayQuestion();
+    });
+
+    $(document).on("click", "#choices-button", function(x){
+        game.selected(x);
+    });
+    
+    $("#reset").on("click", function() {
+        reset();
+    });
+
+    var trivia = [
         {
             question: "What did that obnoxiously smug kid in the beginning say the velociraptor skeleton looked like?",
-            choices: ["A person", "An ostrich", "A six-foot turkey", "His probably absentee father"],
-            answer: 2,
-            photo: "./images/turkey.gif"
+            answers: ["A person", "An ostrich", "A six-foot turkey", "His probably absentee father"],
+            correctAnswer: "A six-foot turkey",
+            image: "assets/images/turkey.gif"
         },
         {
             question: "Where did the scientists at Jurassic Park get the DNA to replicate the dinosaurs?",
-            choices: ["Fossilized bones", "Fossilized eggs", "Loch Ness Monster", "Fossilized amber"],
-            answer: 3,
-            photo: "./images/bingo-dino-dna.gif"
+            answers: ["Fossilized bones", "Fossilized eggs", "Loch Ness Monster", "Fossilized amber"],
+            correctAnswer: "Fossilized amber",
+            image: "assets/images/bingo-dino-dna.gif"
+        },
+        {
+            question: "What is Ian Malcolm's profession?",
+            answers: ["Mathematician", "Philosopher", "Chaotician", "Sex Symbol"],
+            correctAnswer: "Chaotician",
+            image: "assets/images/chaotician.gif"
         }
     ];
-    var timer = 20;
-    var intervalId;
-    var correctCount = 0;
-    var wrongCount = 0;
-    var randomizer = Math.floor(Math.random() * triviaBlocks.length);
+    var game = {
+        currentQuestion: 0,
+        timer: 20,
+        correctCount: 0,
+        wrongCount: 0,
+        countdown: function () {
+            game.timer--;
+            $("#timer").html(game.timer);
 
-    $("#play").on("click", function() {
-        displayQuestion();
-        countdown();
-    })
-    
-    function displayQuestion() {
-        $("question-box").html("<h2>" + triviaBlocks[randomizer].question + "</h2>");
-        $("answers-box").html("<h4>" + triviaBlocks[randomizer].choices + "</h4>");
-        console.log (triviaBlocks[randomizer].question + "   " + triviaBlocks[randomizer].choices + "   " + triviaBlocks[randomizer].answer)
-    }
+            if (game.timer === 0) {
+                game.tooSlow();
+            }
+        },
+        displayQuestion () {
+            timer = setInterval(game.countdown, 1000);
+            $("#question-box").html('<h2>' + trivia[this.currentQuestion].question + '</h2>');
+            for (var i = 0; i < trivia[this.currentQuestion].answers.length; i++) {
+                $("#answers-box").append('<button class="selection-button" id = "choices-button"' + 'data-name="' +trivia[this.currentQuestion].answers[i] + '">' + trivia[this.currentQuestion].answers[i] + '</button>' );
+            }
+        },
+        nextQuestion: function () {
+            game.timer = 20;
+            $("#timer").html(game.timer);
+            game.currentQuestion++;
+            game.displayQuestion();
+        },
+        tooSlow: function() {
+            clearInterval(timer);
+            $("#timer").html(game.timer);
+            $("#answers-box").html("<img src='assets/images/must-go-faster.gif'>");
 
-    function countdown () {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-    }
-    function decrement() {
-        timer--;
-        $("#timer").html("<h3> " + timer + "</h3>");
-        if (timer === 0) {
-            stop();
-            $("answer-box").html("<img src= './images/must-go-faster.gif'>");
+        if (game.currentQuestion === trivia.length-1) {
+            setTimeout(game.update, 5 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 5 * 1000);
         }
+    },
+    update: function() {
+        clearInterval(timer);
+        $("#question-box").html("<img src='../images/crazy.gif'>");
+        $("#answers-box").html("<h3> Number correct: " + correctCount + "</h3>");
+        $("#answers-box").append ("<h3> Number wrong: " + wrongCount + "</h3>");
+    },
+    selected: function (x) {
+        clearInterval(timer);
+        console.log(trivia[this.currentQuestion].correctAnswer + trivia[this.currentQuestion].image);
+        if ($(x.target).data("name") === trivia[this.currentQuestion].correctAnswer){
+            trivia.correctCount++;
+            game.pauseScreen();
+            $("#answers-box").html('<img src=' + trivia[this.currentQuestion].image + '>');
+        } else {
+            trivia.wrongCount++;
+            game.pauseScreen();
+            $("#answers-box").html('<img src=' + trivia[this.currentQuestion].image + '>');
+        }
+    },
+    pauseScreen: function () {
+        clearInterval (timer);
+        if (game.currentQuestion === trivia.length -1) {
+            setTimeout(game.update, 5 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 5 * 1000);
+        };
+        
+        
     }
-    function stop() {
-        clearInterval(intervalId);
-    }
+}
+
 
 });
